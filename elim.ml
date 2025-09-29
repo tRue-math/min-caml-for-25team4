a83bcddc6fe498a9e08a9d972599721388125fa8
Eijiro Sumii <eijiro.sumii@gmail.com>
2025-09-29 17:43:34 +0900

    effect -> (not) pure

--- elim.ml
+++ elim.ml
@@ -1,10 +1,10 @@
 open KNormal
 
-let rec effect = function (* 副作用の有無 (caml2html: elim_effect) *)
-  | Let(_, e1, e2) | IfEq(_, _, e1, e2) | IfLE(_, _, e1, e2) -> effect e1 || effect e2
-  | LetRec(_, e) | LetTuple(_, _, e) -> effect e
-  | App _ | Put _ | ExtFunApp _ -> true
-  | _ -> false
+let rec pure = function (* 副作用の有無 (caml2html: elim_pure) *)
+  | Let(_, e1, e2) | IfEq(_, _, e1, e2) | IfLE(_, _, e1, e2) -> pure e1 && pure e2
+  | LetRec(_, e) | LetTuple(_, _, e) -> pure e
+  | App _ | Put _ | ExtFunApp _ -> false
+  | _ -> true
 
 let rec f = function (* 不要定義削除ルーチン本体 (caml2html: elim_f) *)
   | IfEq(x, y, e1, e2) -> IfEq(x, y, f e1, f e2)
@@ -12,7 +12,7 @@ let rec f = function (* 不要定義削除ルーチン本体 (caml2html: elim_f)
   | Let((x, t), e1, e2) -> (* letの場合 (caml2html: elim_let) *)
       let e1' = f e1 in
       let e2' = f e2 in
-      if effect e1' || S.mem x (fv e2') then Let((x, t), e1', e2') else
+      if not (pure e1') || S.mem x (fv e2') then Let((x, t), e1', e2') else
       (Format.eprintf "eliminating variable %s@." x;
        e2')
   | LetRec({ name = (x, t); args = yts; body = e1 }, e2) -> (* let recの場合 (caml2html: elim_letrec) *)
