6c11f9afec2c16f285c0ef64a969810dc71d18cf
tRue <hi06yo13ri@gmail.com>
2025-11-06 11:34:48 +0900

    Implement `Type.to_string` for better debug

--- type.ml
+++ type.ml
@@ -9,3 +9,20 @@ type t = (* MinCamlの型を表現するデータ型 (caml2html: type_t) *)
   | Var of t option ref
 
 let gentyp () = Var(ref None) (* 新しい型変数を作る *)
+
+let rec to_string = function
+  | Unit -> "Unit"
+  | Bool -> "Bool"
+  | Int -> "Int"
+  | Float -> "Float"
+  | Fun(args, ret) ->
+      let args_str = String.concat ", " (List.map to_string args) in
+      Printf.sprintf "Fun([%s], %s)" args_str (to_string ret)
+  | Tuple(ts) ->
+      let ts_str = String.concat ", " (List.map to_string ts) in
+      Printf.sprintf "Tuple([%s])" ts_str
+  | Array(t) ->
+      Printf.sprintf "Array(%s)" (to_string t)
+  | Var({contents=None}) -> "Var(None)"
+  | Var({contents=Some t}) ->
+      Printf.sprintf "Var(Some(%s))" (to_string t)
