8cf1de438fc4fc768b0506c51aa601677b0016a3
tRue <hi06yo13ri@gmail.com>
2025-11-11 13:52:26 +0900

    Save return address(`%ra`)

--- toysaRas/emit.ml
+++ toysaRas/emit.ml
@@ -183,10 +183,12 @@ and g' oc (dest,e) pos = match dest,e with (* 各命令のアセンブリ生成 
   | NonTail(a), CallCls(x, ys, zs) ->
       g'_args oc [(x, reg_cl)] ys zs pos;
       let ss = stacksize () in
-      if ss > 0 then emit pos oc ["\taddi";reg_sp;reg_sp;imm ss];
+      emit pos oc ["\tsw";reg_ra;reg_sp;imm ss];
+      emit pos oc ["\taddi";reg_sp;reg_sp;imm (ss+4)];
       emit pos oc ["\tlw";reg_cl;reg_cl;"$0"];
       emit pos oc ["\tjalr";reg_ra;reg_cl;"$0"];
-      if ss > 0 then emit pos oc ["\taddi";reg_sp;reg_sp;imm (-ss)];
+      emit pos oc ["\taddi";reg_sp;reg_sp;imm (-ss-4)];
+      emit pos oc ["\tlw";reg_ra;reg_sp;imm ss];
       if List.mem a allregs && a <> regs.(0) then
         emit pos oc ["\taddi";a;regs.(0);"$0"]
       else if List.mem a allfregs && a <> fregs.(0) then
@@ -194,9 +196,11 @@ and g' oc (dest,e) pos = match dest,e with (* 各命令のアセンブリ生成 
   | NonTail(a), CallDir(Id.L(x), ys, zs) ->
       g'_args oc [] ys zs pos;
       let ss = stacksize () in
-      if ss > 0 then emit pos oc ["\taddi";reg_sp;reg_sp;imm ss];
+      emit pos oc ["\tsw";reg_ra;reg_sp;imm ss];
+      emit pos oc ["\taddi";reg_sp;reg_sp;imm (ss+4)];
       emit pos oc ["\tjal";reg_ra;"$"^x];
-      if ss > 0 then emit pos oc ["\taddi";reg_sp;reg_sp;imm (-ss)];
+      emit pos oc ["\taddi";reg_sp;reg_sp;imm (-ss-4)];
+      emit pos oc ["\tlw";reg_ra;reg_sp;imm ss];
       if List.mem a allregs && a <> regs.(0) then
         emit pos oc ["\taddi";a;regs.(0);"$0"]
       else if List.mem a allfregs && a <> fregs.(0) then
